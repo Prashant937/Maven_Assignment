1196b7e2cd2dc58da38a05c070c23912182a7ff9
Prashant937 <prashant.trivedi@knoldus.com>
2021-01-19 21:40:37 +0530

    Commit By Prashant Trivedi, Employee id-1411

---
+++ Assign/string-impl/src/main/java/Application.java
@@ -0,0 +1,9 @@
+public class Application {
+
+    public static void main(String[] args) {
+        StringFunction s=new StringFunction();
+
+        s.revString("Prashant");
+        s.findLength("Prashant");
+    }
+}
